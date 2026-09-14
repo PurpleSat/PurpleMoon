495f5c624a7bc650b4513b2e67ed50a2598fb30a
Kevin T Garland <usd872@outlook.com>
2026-09-14 20:01:34 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -40,9 +40,10 @@ export const useSidebar = () => useContext(SidebarContext);
 
 // 友情链接配置列表
 const FRIEND_LINKS = [
-  { name: '红月搜索', url: 'https://rm.400821.xyz' },
-  { name: 'My-CMS', url: 'https://today.400823.xyz' },
+  { name: '紫月·镜像', url: 'https://image.400821.xyz' },
+  { name: '红月·搜索', url: 'https://rm.400821.xyz' },
   { name: 'My-Cloud', url: 'https://200805.xyz' },
+  { name: 'My-CMS', url: 'https://today.400823.xyz' },
   { name: 'API中转代理', url: 'https://timis.dpdns.org' },
 ];
 
