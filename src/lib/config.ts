a3c2ed831d30b29a38a33d8460136b5707a981e9
Kevin T Garland <usd872@outlook.com>
2026-09-14 11:06:34 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -157,7 +157,7 @@ async function initConfig() {
             SiteName: process.env.SITE_NAME || 'PurpleMoon',
             Announcement:
               process.env.ANNOUNCEMENT ||
-              '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
+              'Welcome to PurpleMoon. Please note that all content available on this site is sourced from the Internet.',
             SearchDownstreamMaxPage:
               Number(process.env.NEXT_PUBLIC_SEARCH_MAX_PAGE) || 5,
             SiteInterfaceCacheTime: fileConfig.cache_time || 7200,
@@ -195,7 +195,7 @@ async function initConfig() {
         SiteName: process.env.SITE_NAME || 'PurpleMoon',
         Announcement:
           process.env.ANNOUNCEMENT ||
-          '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
+          'Welcome to PurpleMoon. Please note that all content available on this site is sourced from the Internet.',
         SearchDownstreamMaxPage:
           Number(process.env.NEXT_PUBLIC_SEARCH_MAX_PAGE) || 5,
         SiteInterfaceCacheTime: fileConfig.cache_time || 7200,
@@ -236,7 +236,7 @@ export async function getConfig(): Promise<AdminConfig> {
     adminConfig.SiteConfig.SiteName = process.env.SITE_NAME || 'PurpleMoon';
     adminConfig.SiteConfig.Announcement =
       process.env.ANNOUNCEMENT ||
-      '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。';
+      'Welcome to PurpleMoon. Please note that all content available on this site is sourced from the Internet.';
     adminConfig.UserConfig.AllowRegister =
       process.env.NEXT_PUBLIC_ENABLE_REGISTER === 'true';
     adminConfig.SiteConfig.ImageProxy =
@@ -317,7 +317,7 @@ export async function resetConfig() {
       SiteName: process.env.SITE_NAME || 'PurpleMoon',
       Announcement:
         process.env.ANNOUNCEMENT ||
-        '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
+        'Welcome to PurpleMoon. Please note that all content available on this site is sourced from the Internet.',
       SearchDownstreamMaxPage:
         Number(process.env.NEXT_PUBLIC_SEARCH_MAX_PAGE) || 5,
       SiteInterfaceCacheTime: fileConfig.cache_time || 7200,
